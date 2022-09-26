ce02238ee552cc68db0d47e74d5eec5b8bc49620
Dmitry Voronkevich <kaducei2020@gmail.com>
2022-09-26 20:36:10 +0300

    1.1 FIX

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const requestURL = "https://api.github.com/search/repositories?q=";
 const searchInput = document.querySelector(".search__input");
 const searchDropdown = document.querySelector(".search__dropdown");
-const repos = document.querySelector(".repos");
+const collection = document.querySelector(".repos");
 
 const debounce = (func, delay) => {
   let inDebounce;
@@ -16,20 +16,20 @@ async function fetchData(value) {
     data.json().then((response) => {
       const repositories = response.items;
       const repFragment = document.createDocumentFragment();
-      repositories.forEach((repo) => {
-        const newElem = document.createElement("li");
-        newElem.textContent = repo.full_name;
-        newElem.addEventListener("click", () => {
+      repositories.forEach((repositoriy) => {
+        const newElement = document.createElement("li");
+        newElement.textContent = repositoriy.full_name;
+        newElement.addEventListener("click", () => {
           createSelectElement(
-            repo.name,
-            repo.owner.login,
-            repo.stargazers_count,
-            repo.html_url
+            repositoriy.name,
+            repositoriy.owner.login,
+            repositoriy.stargazers_count,
+            repositoriy.html_url
           );
           searchDropdown.innerHTML = "";
           searchInput.value = "";
         });
-        repFragment.appendChild(newElem);
+        repFragment.appendChild(newElement);
       });
       searchDropdown.innerHTML = "";
       searchDropdown.appendChild(repFragment);
@@ -45,32 +45,33 @@ inputChange = debounce(inputChange, 500);
 searchInput.addEventListener("input", inputChange);
 
 function createSelectElement(name, owner, stars, link) {
-  const elemFragment = document.createDocumentFragment();
-  const newRep = document.createElement("li");
+  const elementFragment = document.createDocumentFragment();
+  const newRepository = document.createElement("li");
   const repoLink = document.createElement("a");
 
   repoLink.setAttribute("href", link);
   repoLink.setAttribute("target", "_blank");
   repoLink.textContent = name;
 
-  const elemName = document.createElement("h4");
-  elemName.appendChild(repoLink);
-  newRep.appendChild(elemName);
+  const elementName = document.createElement("h4");
+  elementName.appendChild(repoLink);
+  newRepository.appendChild(elementName);
 
-  const elemOwner = document.createElement("div");
-  elemOwner.textContent = owner;
-  newRep.appendChild(elemOwner);
+  const elementOwner = document.createElement("div");
+  elementOwner.textContent = owner;
+  newRepository.appendChild(elementOwner);
 
-  const elemStars = document.createElement("div");
-  elemStars.textContent = "☆" + stars;
-  newRep.appendChild(elemStars);
+  const elementStars = document.createElement("div");
+  elementStars.textContent = "☆" + stars;
+  newRepository.appendChild(elementStars);
 
-  const deleteBtn = document.createElement("button");
-  newRep.appendChild(deleteBtn);
-  deleteBtn.addEventListener("click", () => {
-    newRep.classList.add("cancel");
+  const deleteButton = document.createElement("button");
+  newRepository.appendChild(deleteButton);
+  deleteButton.addEventListener("click", () => {
+    newRepository.onclick = null;
+    newRepository.remove();
   });
 
-  elemFragment.appendChild(newRep);
-  repos.appendChild(elemFragment);
+  elementFragment.appendChild(newRepository);
+  collection.appendChild(elementFragment);
 }
